54b94b51a11e4b8a3a68ff9e68fead308c96f13f
John Meeks <jpmeeks73@yahoo.com>
2026-05-31 21:18:25 -0500

    Update 02_clean_data.py

--- python/02_clean_data.py
+++ python/02_clean_data.py
@@ -1,1 +1,19 @@
+#3. Clean Column Names
 
+df_clean = df.copy()
+
+df_clean.columns = (
+    df_clean.columns
+    .str.strip()
+    .str.lower()
+    .str.replace(" ", "_")
+    .str.replace("(", "", regex=False)
+    .str.replace(")", "", regex=False)
+    .str.replace("/", "_")
+)
+
+print(df_clean.columns)
+
+# 4. Check for Missing Values
+
+print(df_clean.isnull().sum())
